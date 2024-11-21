1e039af459bc1aea80ebdc4b2e19cd53f7a8d69e
dimasikshemetoff <dshemetoff@gmail.com>
2024-11-21 15:57:47 +0300

    second commit

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.burger').addEventListener('click', (e) => {
         document.querySelector('.burger').classList.add('open');
+        document.querySelector('.burger_menu').classList.add('full');
+        
+    });
+    document.querySelector('.burger_menu img').addEventListener('click', (e) => {
+        document.querySelector('.burger').classList.remove('open');
+        document.querySelector('.burger_menu').classList.remove('full');
         
     })
 });

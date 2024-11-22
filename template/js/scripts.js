028bbc3177be3e6a344bf22798a78783a7f87a5d
dimasikshemetoff <dshemetoff@gmail.com>
2024-11-23 00:31:14 +0300

    verstka

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.burger').addEventListener('click', (e) => {
         document.querySelector('.burger').classList.add('open');
         document.querySelector('.burger_menu').classList.add('full');
+        document.querySelector('body').classList.add('overflow');
         
     });
     document.querySelector('.burger_menu img').addEventListener('click', (e) => {
         document.querySelector('.burger').classList.remove('open');
         document.querySelector('.burger_menu').classList.remove('full');
-        
+        document.querySelector('body').classList.remove('overflow');
     })
 });
